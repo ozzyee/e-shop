23729400cb9949ecff87c6068d2e3a4664adee3e
ozzyee <62845796+ozzyee@users.noreply.github.com>
2022-05-20 14:30:01 +0100

    cart

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Carousel from "../components/carousel";
+import { useShoppingListContent } from "../context/shoping-list";
 
-const fakeData = [
+const images = [
    "/alyssa-strohmann-TS--uNw-JqE-unsplash.jpg",
    "atikh-bana-_KaMTEmJnxY-unsplash.jpg",
    "cesar-la-rosa-HbAddptme1Q-unsplash.jpg",
@@ -11,5 +12,11 @@ const fakeData = [
 ];
 
 export default function Home() {
-   return <Carousel data={fakeData} />;
+   const { addItem, deleteItem } = useShoppingListContent();
+
+   return (
+      <>
+         <Carousel data={images} />
+      </>
+   );
 }
